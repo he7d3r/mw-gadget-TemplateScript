f3551df470a91af3095479fe481f169d4c0b3c44
Helder <he7d3r@gmail.com>
2009-06-02 18:17:25 +0000

    Add a few search/replace rules
    Copied from
    https://en.wikisource.org/w/index.php?title=User:Pathoschild/standardise.js&oldid=434174

--- src/TemplateScript.js
+++ src/TemplateScript.js
@@ -10,9 +10,33 @@ importScriptURI('http://meta.wikimedia.org/w/index.php?title=User:Pathoschild/Sc
 // links to the sidebar menu. The function name is the function defined in rfmscripts() below.
 function rmflinks() {
 	regexTool('Custom regex','custom()'); // a default tool which performs regex input in a dynamic form
+	regexTool('Padronização','padronizar()'); // a default tool which performs regex input in a dynamic form
 }
  
 /* scripts */
 // Below, define the functions linked to from rmflinks() above. These functions can use any JavaScript,
 // but there is a set of simplified tools documented at
 // http://meta.wikimedia.org/wiki/User:Pathoschild/Script:Regex_menu_framework .
+
+function padronizar() {
+	/*******************
+	*** miscellaneous cleanup
+	*******************/
+	/*   templates */
+	regex(/{{\s*(?:msg:|template:)?([^}]+)}}/ig,'{{$1}}');
+ 
+	/* syntax */
+	// headers
+	regex(/\n*^(=+)\s*(.*?)\s*\1\s*/mig,'\n\n$1$2$1\n'); // whitespace
+	regex(/=\n+=/ig,'=\n='); // fix consecutive headers
+ 
+	// categories
+	regex(/\[\[\s*category\s*:\s*([^\|\]]+)(?:\s*(\|)([^\]]*))?\s*\]\]/ig,'[[category:$1$2$3]]');
+ 
+	//links
+	regex(/\[\[\s*([^\|\]]+?)\s*(?:(\|)\s*([^\]]+?)\s*)?\]\]/ig,'[[$1$2$3]]'); // redundant starting and ending whitespace
+	regex(/\[\[([^\|\]]+?)\s*\|\s*\1\]\]/ig,'[[$1]]'); // redundant link text
+	regex(/\[\[([^\|\]]+?)_/ig,'[[$1 ',5); // underscores
+ 
+	regex(/^([*#:]+)\s*/mig,'$1 ');	doaction('diff');
+}
