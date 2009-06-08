e67abbcd6b293999e4cfca50278dca2d5c658221
Helder <he7d3r@gmail.com>
2009-06-08 17:29:44 +0000

    Split the rules into individual links

--- src/TemplateScript.js
+++ src/TemplateScript.js
@@ -9,30 +9,47 @@ importScriptURI('http://meta.wikimedia.org/w/index.php?title=User:Pathoschild/Sc
 // In the function below, add more lines like "regexTool('link text','function_name()')" to add
 // links to the sidebar menu. The function name is the function defined in rfmscripts() below.
 function rmflinks() {
-	regexTool('Custom regex','custom()'); // a default tool which performs regex input in a dynamic form
-	regexTool('Padronização','padronizar()'); // a default tool which performs regex input in a dynamic form
+	regexTool('Formatar cabeçalhos','format_cab()');
+	regexTool('Formatar predefinições','format_predef()');
+	regexTool('Formatar categorias','format_cat()');
+	regexTool('Formatar listas','format_list()');
+	regexTool('Formatar links','format_links()');
+	regexTool('Regex no sumário','usando_regex()');
+
+	regexTool('Custom regex','custom()'); // Uma ferramenta padrão que executa regex em um formulário dinâmico
 }
  
 /* scripts */
-// Below, define the functions linked to from rmflinks() above. These functions can use any JavaScript,
-// but there is a set of simplified tools documented at
-// http://meta.wikimedia.org/wiki/User:Pathoschild/Script:Regex_menu_framework .
+// Abaixo, defina as funções referenciadas a partir de rmflinks(), logo acima. Estas funções podem usar qualquer JavaScript,
+// mas há um conjunto de ferramentas simplificadas documentadas em
+// http://meta.wikimedia.org/wiki/User:Pathoschild/Script:Regex_menu_framework.
 
-function padronizar() {
-	/*******************
-	*** Limpezas gerais na sintaxe wiki
-	*******************/
-	/* Predefinições */
-	regex(/{{\s*(?:msg:|template:)?([^}]+)}}/ig,'{{$1}}');
- 
-	// Cabeçalhos
+function format_cab() {
 	regex(/\n*^(=+)\s*(.*?)\s*\1\s*/mig,'\n\n$1$2$1\n'); // +quebra de linha antes de =, -espaços entre = e o título da seção
 	regex(/=\n+=/ig,'=\n='); // -quebras de linha entre cabeçalhos consecutivos
- 
-	// Categorias
+	setreason('format. cabeçalhos', 'append');
+	doaction('diff');
+}
+
+function format_predef() {
+	regex(/{{\s*(?:msg:|template:)?([^}]+)}}/ig,'{{$1}}');
+	setreason('format. predefs', 'append');
+	doaction('diff');
+}
+
+function format_cat() {
 	regex(/\[\[\s*(?:category|categoria)\s*:\s*([^\|\]]+)(?:\s*(\|)([^\]]*))?\s*\]\]/ig,'[[Categoria:$1$2$3]]');
- 
-	// Ligações
+	setreason('format. categorias', 'append');
+	doaction('diff');
+}
+
+function format_list() {
+	regex(/^([*#:]+)\s*/mig,'$1 '); //apenas 1 espaço entre *, # ou : e o texto da lista
+	setreason('format. listas', 'append');
+	doaction('diff');
+}
+
+function format_links() {
 	regex(/\[\[\s*([^\|\]]+?)\s*(?:(\|)\s*([^\]]+?)\s*)?\]\]/ig,'[[$1$2$3]]'); // -espaços redundantes
 	regex(/\[\[([^\|\]]+?)\s*\|\s*\1\]\]/ig,'[[$1]]');   //  [[Texto|Texto]] → [[Texto]]
 	regex(/\[\[\s*\/\s*([^\|\]]+?)\s*\|\s*\1\s*\]\]/ig,'[[/$1/]]'); // [[/Texto|Texto]] → [[/Texto/]]
@@ -45,10 +62,10 @@ function padronizar() {
 
 	regex(/\[\[([^\|\]]+?)_/ig,'[[$1 ',5); // troca de underscores por espaços nas ligações
 
-	// Listas
-	regex(/^([*#:]+)\s*/mig,'$1 '); //apenas 1 espaço entre *, # ou : e o texto da lista
-
-	setreason('Padronização, atualizações e limpeza da sintaxe usando [[meta:User:Pathoschild/Scripts/Regex menu framework|regex]] (em fase de testes)', 'append');
-
+	setreason('simplificando links', 'append');
 	doaction('diff');
+}
+
+function usando_regex() {
+	setreason('[usando [[meta:User:Pathoschild/Scripts/Regex menu framework|regex]]]', 'append');
 }
