406c62a19bd4753bed77b9a0e76316124a31c6a4
Helder <he7d3r@gmail.com>
2009-10-30 11:38:25 +0000

    Revert the previous change and improve the old regex

--- src/TemplateScript.js
+++ src/TemplateScript.js
@@ -217,7 +217,7 @@ function format_cab() {
 	if ( "Livro_de_receitas" == wgBookName ){
 		regex(/==\s*[^\n]+\s+-\s+(\d+)\s*==/ig, '== Receita $1 ==');
 		regex(/==='''Ingredientes e Preparo:'''===/ig, '=== Ingredientes ===');
-		regex(/:\s*'''Preparo:'''\n\n/ig, '=== Preparo ===\n');
+		regex(/:\s*'''(?:Modo\s+de\s+)?Preparo:\s*'''/ig, '=== Preparo ===');
 		regex(/ --\n/ig, ';\n');
 	}
 
